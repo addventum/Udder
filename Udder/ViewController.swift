e9788ba9815c3f0b43e2791c8d4bd1f5e6b82a72
Ross <adventum@icloud.com>
2020-04-10 16:57:02 +0300

    Add label

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Adventum on 10.04.20.
+//  Copyright © 2020 Adventum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
